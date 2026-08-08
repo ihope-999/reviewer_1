c9985bbf8a7de29c908c365ddb6aa975d1492d66
Umut <umut.clsc01@outlook.com>
2026-08-08 21:58:23 +0300

    search result class and interface added

---
+++ review_page/Domain/LocationDomain/Core/Models/PlaceSearchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace review_page.Domain.LocationDomain.Core.Models
+{
+    public record PlaceSearchResult
+    {
+        public string PlaceId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ReviewURL { get; set; }
+    }
+}
