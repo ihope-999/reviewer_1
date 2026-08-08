b67238415fe23e7dfd78bf3e3e3563a9d5615235
Umut <umut.clsc01@outlook.com>
2026-08-08 21:51:03 +0300

    interface for export and review class added

---
+++ review_page/Domain/LocationDomain/Core/Data/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace review_page.Domain.LocationDomain.Core.Data
+{
+    public class Review
+    {
+        public Review() { }
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public string PlaceId { get; set; }
+        public string RecipientName { get; set; }
+        public string ReviewURL { get; set; }
+        public string RecipientEmail { get; set; }
+        public bool EmailSent { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? QrCodeB64 { get; set; }
+
+    }
+}
---
+++ review_page/Domain/LocationDomain/Core/Interfaces/IExportService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using review_page.Domain.LocationDomain.Core.Data;
+
+namespace review_page.Domain.LocationDomain.Core.Interfaces
+{
+    public interface IExportService
+    {
+        public byte[] ExportToExcel(IEnumerable<Review> requests);
+        public byte[] ExportToPdf(IEnumerable<Review> list);
+
+
+    }
+}
