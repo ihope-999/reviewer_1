82a3db2a8f39bd054b64f211b132b437103e4e9f
Umut <umut.clsc01@outlook.com>
2026-08-11 16:15:47 +0300

    qrcode service

---
+++ review_page/Domain/LocationDomain/Core/Interfaces/IQrCodeService.cs
@@ -0,0 +1,7 @@
+﻿namespace review_page.Domain.LocationDomain.Core.Interfaces
+{
+    public interface IQrCodeService
+    {
+        public byte[] GenerateQrCode(string url, int pixelsPerModule = 10);
+    }
+}
---
+++ review_page/Domain/LocationDomain/Services/QrCodeService.cs
@@ -0,0 +1,21 @@
+﻿using QRCoder;
+using review_page.Domain.LocationDomain.Core.Interfaces;
+
+namespace review_page.Domain.LocationDomain.Services
+{
+    public class QrCodeService : IQrCodeService
+    {
+
+
+        public byte[] GenerateQrCode(string url, int pixelsPerModule = 10)
+        {
+            using var generator = new QRCodeGenerator();
+            using var data = generator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            using var code = new PngByteQRCode(data);
+            return code.GetGraphic(pixelsPerModule);
+        }
+
+        public string GenerateBase64(string url) =>
+            Convert.ToBase64String(GenerateQrCode(url));
+    }
+}
