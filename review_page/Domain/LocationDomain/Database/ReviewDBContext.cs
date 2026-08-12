e76dd986bd614b16d8beb992d25f94f6150cc888
Umut <umut.clsc01@outlook.com>
2026-08-12 18:27:56 +0300

    reviewdb added

---
+++ review_page/Domain/LocationDomain/Database/ReviewDBContext.cs
@@ -0,0 +1,22 @@
+﻿using review_page.Domain.LocationDomain.Core.Data;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+namespace review_page.Domain.LocationDomain.Database
+{
+    public class ReviewDBContext : DbContext
+    {
+        public ReviewDBContext(DbContextOptions<ReviewDBContext> options) : base(options) { }
+
+        public DbSet<Review> ReviewDB { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+
+    }
+}
