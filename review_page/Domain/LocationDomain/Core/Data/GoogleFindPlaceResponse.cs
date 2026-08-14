0a2c5123046d68dbd7157e08bb936982ecdda44b
Umut <umut.clsc01@outlook.com>
2026-08-14 14:24:47 +0300

    find place response added

---
+++ review_page/Domain/LocationDomain/Core/Data/GoogleFindPlaceResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace review_page.Domain.LocationDomain.Core.Data
+{
+    public record GooglePlaceCandidate
+    {
+        [property: JsonPropertyName("place_id")]
+        public string PlaceId;
+
+        [property: JsonPropertyName("name")]
+        public string Name;
+
+        [property: JsonPropertyName("formatted_address")]
+        public string FormattedAddress;
+
+    }
+
+
+    public record GoogleFindPlaceResponse
+    {
+        [property: JsonPropertyName("candidates")]
+        public List<GooglePlaceCandidate>? Candidates;
+        [JsonPropertyName("status")]
+        public string? Status { get; set; }
+    }
+}
